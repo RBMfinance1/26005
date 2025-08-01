0207514cd35c39717e1269bf8d6fdd29ac464a86
RBMfinance1 <finance1@royalbrandmaker.fr>
2025-08-01 16:26:00 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration de l'application
 const APP_CONFIG = {
   // Hash SHA-256 du mot de passe "262626"
-  PASSWORD_HASH: "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
+  PASSWORD_HASH: "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
   
   // Utilisateurs autorisés (optionnel)
   ALLOWED_USERS: ["admin", "user1", "user2"],
